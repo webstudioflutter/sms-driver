6222443d1515f673f201e1b5c7ba7a6dc1815e96
Swastika Shrestha <your.email@example.com>
2024-11-05 13:26:58 +0545

    new_bill_updates

---
+++ lib/features/bill/bill_main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BillMain extends StatelessWidget {
+  const BillMain({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Bill Upload"),
+      ),
+      body: Container(),
+    );
+  }
+}
